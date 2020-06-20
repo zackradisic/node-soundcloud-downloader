4a28f23d09d4f70d00080618809d8e666ca7c79d
zackradisic <zack@theradisic.com>
2020-06-20 05:10:13 -0400

    add babel, eslint, jest, and m3u8stream

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    es2020: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module'
+  },
+  plugins: [
+  ],
+  rules: {
+    indent: ['error', 2]
+  }
+}
