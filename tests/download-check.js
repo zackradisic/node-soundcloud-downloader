21e308629a11b66b1ac5e1646042dc2ec28a76a1
zackradisic <zack@theradisic.com>
2020-06-25 21:53:59 -0400

    move file download to download-check test

--- tests/download-check.js
+++ tests/download-check.js
@@ -1,10 +1,21 @@
 const fs = require('fs')
-require('../example/download-file')
+const scdl = require('../')
 
-const stats = fs.statSync('audio.mp3')
-fs.unlinkSync('audio.mp3')
-if (stats.size !== 3616181) {
-  process.exit(1)
-}
+scdl.download('https://soundcloud.com/monsune_inc/outta-my-mind', process.env.CLIENT_ID)
+  .then(stream => {
+    stream.pipe(fs.createWriteStream('audio.mp3'))
 
-process.exit(0)
+    stream.on('finish', () => {
+      const stats = fs.statSync('audio.mp3')
+      fs.unlinkSync('audio.mp3')
+      if (stats.size !== 3616181) {
+        process.exit(1)
+      }
+
+      process.exit(0)
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    process.exit(1)
+  })
