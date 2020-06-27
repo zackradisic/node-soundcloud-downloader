96f407dfebdfe84a3cf0c4237a98f07e1c2197f3
Rahul Tarak <rahultarak12345@gmail.com>
2020-06-27 15:14:10 +0530

    Added Automated Testing for Downloading Files

---
+++ tests/downloadCheck.test.js
@@ -0,0 +1,42 @@
+/**
+ * @jest-environment node
+ */
+
+/**
+ *  This file tests the actual download of the a song, without mocking axios
+ *
+ * Author : Rahul Tarak
+ *
+ */
+
+const scdl = require('../')
+const fileType = require('file-type')
+let downloadedFile
+
+describe('Real Download Tests', () => {
+  beforeAll(async () => {
+    if (process.env.CLIENT_ID) {
+      downloadedFile = await scdl.download(
+        'https://soundcloud.com/monsune_inc/outta-my-mind',
+        process.env.CLIENT_ID
+      )
+    } else {
+      console.error('Client ID Undefined')
+      process.exit(1)
+    }
+    // console.log(downloadedFile)
+  })
+  it('Stream is Defined', () => {
+    expect(downloadedFile).toBeDefined()
+  })
+  it('Check File Type is Mpeg', async () => {
+    const type = await fileType.fromStream(downloadedFile)
+    expect(type).toBeDefined()
+    expect(type.mime).toBe('audio/mpeg')
+  })
+  it('No Errors in Stream', () => {
+    downloadedFile.on('error', (err) => {
+      expect(err).toBeFalsy()
+    })
+  })
+})
