803a7cb83afbb90d0618a36361b6734b00eb3b70
zackradisic <zack@theradisic.com>
2020-08-17 05:40:03 -0400

    revert change

--- example/get-info.js
+++ example/get-info.js
@@ -1,6 +1,6 @@
 const scdl = require('../')
 const constants = require('./constants')
 
-scdl.getSetInfo('https://soundcloud.com/user-845046062/sets/playlist', constants.clientID)
+scdl.getInfo(constants.url, constants.clientID)
   .then(info => console.log(info))
   .catch(err => console.log(err.message))
