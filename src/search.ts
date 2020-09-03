afa70fed7c2f9e602efad351a5f1453a6faeae8c
zackradisic <zack@theradisic.com>
2020-09-03 18:08:23 -0400

    related() only works for tracks

--- src/search.ts
+++ src/search.ts
@@ -21,16 +21,16 @@ export type RelatedResponse<T> = {
 
 export type SearchResponseAll = SearchResponse<User | SetInfo | TrackInfo>
 
-export type SoundcloudResource = 'tracks' | 'people' | 'albums' | 'playlists'
+export type SoundcloudResource = 'tracks' | 'users' | 'albums' | 'playlists'
 
 /** @internal */
 export const search = async (type: SoundcloudResource | 'all', query: string, clientID?: string): Promise<SearchResponseAll> => {
-  const { data } = await axios.get(`${baseURL}${type === 'all' ? '' : `/${type}/`}?client_id=${clientID}&q=${query}`)
+  const { data } = await axios.get(`${baseURL}${type === 'all' ? '' : `/${type}`}?client_id=${clientID}&q=${query}`)
   return data as SearchResponseAll
 }
 
 /** @internal */
-export const related = async <T extends TrackInfo | User | SetInfo> (type: SoundcloudResource, id: number, limit = 10, offset = 0, clientID: string): Promise<RelatedResponse<T>> => {
-  const { data } = await axios.get(`https://api-v2.soundcloud.com/${type}/${id}/related?client_id=${clientID}&offset=${offset}&limit=${limit}`)
+export const related = async <T extends TrackInfo> (id: number, limit = 10, offset = 0, clientID: string): Promise<RelatedResponse<T>> => {
+  const { data } = await axios.get(`https://api-v2.soundcloud.com/tracks/${id}/related?client_id=${clientID}&offset=${offset}&limit=${limit}`)
   return data as RelatedResponse<T>
 }
