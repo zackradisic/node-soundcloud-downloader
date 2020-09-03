8897fb41c782e4b4034d62e368a83d216e9f8929
zackradisic <zack@theradisic.com>
2020-09-03 18:07:45 -0400

    add search example

---
+++ example/search.js
@@ -0,0 +1,5 @@
+const scdl = require('../')
+
+scdl.search('tracks', 'redbone childish gambino')
+  .then(results => results.collection.forEach(track => console.log(track.title)))
+  .catch(err => console.log(err))
