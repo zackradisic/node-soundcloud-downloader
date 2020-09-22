811a95195c760634cf523e771ffaf15e624b9ec0
zackradisic <zack@theradisic.com>
2020-09-22 17:58:08 -0400

    add comment to example

--- example/constants.js
+++ example/constants.js
@@ -1,4 +1,4 @@
 module.exports = {
   url: 'https://soundcloud.com/monsune_inc/outta-my-mind',
-  clientID: process.env.CLIENT_ID
+  clientID: process.env.CLIENT_ID // remember that client ID is optional
 }
