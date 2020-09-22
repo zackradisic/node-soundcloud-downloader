c4a2a7de0b0a6a4d0b5a79db9160516b6f7ed59f
zackradisic <zack@theradisic.com>
2020-09-22 17:56:02 -0400

    created appendURL function (solves query parameter issues)

--- dist/util.js
+++ dist/util.js
@@ -1,7 +1,8 @@
 "use strict";
-/** @internal @packageDocumentation */
 exports.__esModule = true;
-exports.handleRequestErrs = void 0;
+exports.appendURL = exports.handleRequestErrs = void 0;
+/** @internal @packageDocumentation */
+var url_1 = require("url");
 exports.handleRequestErrs = function (err) {
     if (!err.response)
         return err;
@@ -12,4 +13,16 @@ exports.handleRequestErrs = function (err) {
     if (err.response.status === 404)
         err.message += ', could not find the song... it may be private - check the URL';
     return err;
+};
+exports.appendURL = function (url) {
+    var params = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        params[_i - 1] = arguments[_i];
+    }
+    var u = new url_1.URL(url);
+    params.forEach(function (val, idx) {
+        if (idx % 2 === 0)
+            u.searchParams.append(val, params[idx + 1]);
+    });
+    return u.href;
 };
--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,5 @@
 /** @internal @packageDocumentation */
-
+import { URL } from 'url'
 import { AxiosError } from 'axios'
 
 export const handleRequestErrs = (err: AxiosError) => {
@@ -9,4 +9,12 @@ export const handleRequestErrs = (err: AxiosError) => {
   if (err.response.status === 401) err.message += ', is your Client ID correct?'
   if (err.response.status === 404) err.message += ', could not find the song... it may be private - check the URL'
   return err
+}
+
+export const appendURL = (url: string, ...params: string[]) => {
+  const u = new URL(url)
+  params.forEach((val, idx) => {
+    if (idx % 2 === 0) u.searchParams.append(val, params[idx + 1])
+  })
+  return u.href
 }
