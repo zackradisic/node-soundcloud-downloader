487051f9ec48870b245075f9a094ed7e4b65f476
Zack Radisic <zack@theradisic.com>
2020-11-06 21:39:11 -0500

    remove example playlist

--- example/get-info.js
+++ example/get-info.js
@@ -1,6 +1,6 @@
 const scdl = require('../')
 const constants = require('./constants')
 
-scdl.getSetInfo('https://soundcloud.com/zack-radisic-103764335/sets/test', constants.clientID)
+scdl.getSetInfo('adfkjhalskdjfhlas', constants.clientID)
   .then(info => console.log(info))
   .catch(err => console.log(err))
