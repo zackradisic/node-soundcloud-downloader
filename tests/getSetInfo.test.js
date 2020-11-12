ec40e855b10e8864a67abe1fddd96c8158b9199d
Zack Radisic <zack@theradisic.com>
2020-11-11 23:51:38 -0500

    test for playlist with tracks > 55

--- tests/getSetInfo.test.js
+++ tests/getSetInfo.test.js
@@ -7,10 +7,11 @@ import scdl from '../'
 describe('getSetInfo()', () => {
   describe('returns valid SetInfo', () => {
     let info
-
+    let infoLarge
     beforeAll(async () => {
       try {
         info = await scdl.getSetInfo('https://soundcloud.com/user-845046062/sets/playlist')
+        infoLarge = await scdl.getSetInfo('https://soundcloud.com/ilyanaazman/sets/best-of-mrrevillz')
       } catch (err) {
         console.log(err)
         process.exit(1)
@@ -26,6 +27,20 @@ describe('getSetInfo()', () => {
     })
 
     it('returns SetInfo with every track containing full information', () => {
+      for (const track of info.tracks) {
+        expect(track.title).toBeDefined()
+      }
+    })
+
+    it('returns SetInfo for playlist with tracks > 55', () => {
+      expect(infoLarge).toBeDefined()
+      expect(info.title).toBeDefined()
+      expect(typeof info.title).toBe('string')
+      expect(info.tracks).toBeDefined()
+      expect(typeof info.tracks).toBe('object')
+    })
+
+    it('returns SetInfo for playlist with every track containing information for playlist with tracks > 55', () => {
       for (const track of info.tracks) {
         expect(track.title).toBeDefined()
       }
