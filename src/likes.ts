95f808e322d0523435af48fee9eb38429ba42f8e
Zack Radisic <zack@theradisic.com>
2020-11-24 21:59:58 -0500

    increment limit by 1 because for some reason soundcloud returns limit - 1 likes

--- src/likes.ts
+++ src/likes.ts
@@ -12,6 +12,7 @@ export interface Like {
 }
 
 export const getLikes = async (id: number, clientID: string, axiosInstance: AxiosInstance, limit = 10, offset = 0): Promise<PaginatedQuery<Like>> => {
+  limit = limit + 1 // For some reason SoundCloud returns limit - 1, but only for likes??
   const u = appendURL(`https://api-v2.soundcloud.com/users/${id}/likes`, 'client_id', clientID, 'limit', '' + limit, 'offset', '' + offset)
 
   const { data } = await axiosInstance.get(u)
