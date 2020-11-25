7cca9e20e8e813c19c1638b212b94b09de59d424
Zack Radisic <zack@theradisic.com>
2020-11-25 17:29:13 -0500

    mark as internal

--- src/user.ts
+++ src/user.ts
@@ -2,6 +2,7 @@ import { AxiosInstance } from 'axios'
 import { User } from './info'
 import { appendURL, resolveURL } from './util'
 
+/** @internal */
 export const getUser = async (url: string, clientID: string, axiosInstance: AxiosInstance): Promise<User> => {
   const u = appendURL(resolveURL, 'url', url, 'client_id', clientID)
   const { data } = await axiosInstance.get(u)
