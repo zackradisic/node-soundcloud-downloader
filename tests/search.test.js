1df5db1c90c67be2d31b8aff1a268cf5e1cf9f5d
Zack Radisic <zack@theradisic.com>
2021-01-21 09:57:54 +0300

    refactor search test for efficiency

--- tests/search.test.js
+++ tests/search.test.js
@@ -5,61 +5,31 @@
 import scdl from '../'
 
 describe('search()', () => {
-  it('returns a valid search object', async done => {
+  it('returns a valid search object and collection length equal or less than limit and next_href pagination works', async done => {
     try {
       const query = 'borderline tame impala'
       const types = ['all', 'tracks', 'users', 'albums', 'playlists']
 
       types.forEach(async (type, idx) => {
         try {
-          const searchResponse = await scdl.search({
+          let searchResponse = await scdl.search({
             query,
-            resourceType: types[idx]
+            resourceType: types[idx],
+            limit: 5
           })
-          const keys = ['collection', 'total_results', 'query_urn'].forEach(key => expect(searchResponse[key]).toBeDefined())
+          let keys = ['collection', 'total_results', 'query_urn'].forEach(key => expect(searchResponse[key]).toBeDefined())
+          expect(searchResponse.collection.length).toBeLessThanOrEqual(5)
+
+          searchResponse = await scdl.search({
+            nextHref: searchResponse.next_href
+          })
+          keys = ['collection', 'total_results', 'query_urn'].forEach(key => expect(searchResponse[key]).toBeDefined())
           done()
         } catch (err) {
           console.error(err)
           done(err)
         }
       })
-    } catch (err) {
-      console.error(err)
-      done(err)
-    }
-  })
-
-  it('next_href pagination works', async done => {
-    try {
-      try {
-        let response = await scdl.search({
-          query: 'redbone childish gambino',
-          resourceType: 'tracks'
-        })
-        response = await scdl.search({
-          nextHref: response.next_href
-        })
-        const keys = ['collection', 'total_results', 'query_urn'].forEach(key => expect(response[key]).toBeDefined())
-        done()
-      } catch (err) {
-        console.error(err)
-        done(err)
-      }
-    } catch (err) {
-      console.error(err)
-      done(err)
-    }
-  })
-
-  it('resource count is equal to limit', async done => {
-    try {
-      const query = 'borderline tame impala'
-      const limit = 15
-      const offset = 0
-
-      const searchResponse = await scdl.search({ query, limit, offset, resourceType: 'tracks' })
-      expect(searchResponse.collection.length).toEqual(limit)
-      done()
     } catch (err) {
       console.error(err)
       done(err)
