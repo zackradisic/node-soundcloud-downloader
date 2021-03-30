de7a0785abfeb483a0f36ab6d0fabf9573c5fd2b
Zack Radisic <zack@theradisic.com>
2021-03-31 01:18:38 +0300

    update likes example

--- example/likes.js
+++ example/likes.js
@@ -1,7 +1,5 @@
 const scdl = require('../').default
 
-// scdl.getLikes({
-//   profileUrl: 'https://soundcloud.com/deathcraft-200216746'
-// }).then(({ collection }) => collection.forEach(like => console.log(like.track.id === 13867919, like.track.permalink_url, like.track.downloadable))).catch(err => console.log(err))
-
-scdl.download('https://soundcloud.com/dakota-perez-7/omfg-mashup-hello-i-love-you-yeah-ice-cream-and-wonderful').then(track => { console.log('hi\n\n\n'); console.log(track) })
+scdl.getLikes({
+  profileUrl: 'https://soundcloud.com/sfsdjfhkhs'
+}).then(({ collection }) => collection.forEach(like => console.log(like.track)))
