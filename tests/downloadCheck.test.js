99d2c5a0483df4eba7a7a3a3cea5304ca7857bac
Zack Radisic <zack@theradisic.com>
2021-03-31 01:18:25 +0300

    remove broken link from test

--- tests/downloadCheck.test.js
+++ tests/downloadCheck.test.js
@@ -21,9 +21,6 @@ describe('Real Download Tests', () => {
     try {
       downloadedFile = await scdl.download(
         'https://soundcloud.com/monsune_inc/outta-my-mind')
-      downloadedFile2 = await scdl.download(
-        'https://soundcloud.com/moccioso/01_wav'
-      )
     } catch (err) {
       console.error(err)
       process.exit(1)
